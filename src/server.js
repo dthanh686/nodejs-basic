05a525f27c7fd2934df48bf4cb98b38171201df1
thanhtd <alimodosay@gmail.com>
2024-03-06 23:20:59 +0700

    update 03/06/2024

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 // const express = require('express')
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ?? 3000;
 
 configViewEngine(app);
 
